0694737c26806982a81208646d696539fdb071ba
costa1wilson <wilsondejesuscosta@gmail.com>
2020-08-25 12:03:16 -0300

    organizando Projeto

---
+++ app/routes/app.routes.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const {join}= require('path');
+
+const filePath = join(__dirname,'users.json');
+
+
+const getUsers = ()=>{
+const data = fs.existsSync(filePath)? fs.readFileSync(filePath):[]
+
+try {
+    return JSON.parse(data);
+} catch (error) {
+    return []
+}
+
+
+}
+
+const saveUsers = (users)=>{
+    return fs.writeFileSync(filePath,JSON.stringify(users,null,'\t'));
+}
+
+const usersRoutes = (app)=>{
+app().route('/users/:id?')
+.get((req,res)=>{
+    const users = getUsers();
+   res.send(users); 
+})
+.post((req,res)=>{
+    const users = getUsers();
+    users.push(req.body);
+    saveUsers(users);
+    res.status(200).send('ok');
+})
+.put((req,res)=>{
+const users = getUsers();
+saveUsers(users.map(user=>{
+    if(user.id === req.params.id){
+        return{
+            ...user,
+            ...  req.body
+        }
+    }
+    return user;
+}))
+res.status(200).send('ok');
+})
+.delete((req,res)=>{
+
+    const users = getUsers();
+    saveUsers(users.filter(user=>user.id !== req.params.id));
+    res.status(200).send(ok);
+
+})
+
+}
+
+module.exports = usersRoutes;
+
+
